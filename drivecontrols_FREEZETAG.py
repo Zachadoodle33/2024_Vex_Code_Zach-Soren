3b29b4ba0ffbd4cfa5bd1e1c10cbaa4298341e69
Zach Dubowy <91037068+Zachadoodle33@users.noreply.github.com>
2024-11-21 14:29:06 -0800

    Create drivecontrols_FREEZETAG.py
    Added the basic movement and logic for freeze tag

---
+++ drivecontrols_FREEZETAG.py
@@ -0,0 +1,149 @@
+#region VEXcode Generated Robot Configuration
+from vex import *
+import urandom
+
+# Brain should be defined by default
+brain=Brain()
+
+# Robot configuration code
+controller_1 = Controller(PRIMARY)
+left_drive_smart = Motor(Ports.PORT11, GearSetting.RATIO_18_1, False)
+right_drive_smart = Motor(Ports.PORT12, GearSetting.RATIO_18_1, True)
+drivetrain = DriveTrain(left_drive_smart, right_drive_smart, 319.19, 295, 40, MM, 1)
+bumper_a = Bumper(brain.three_wire_port.a)
+bumper_b = Bumper(brain.three_wire_port.b)
+
+
+# wait for rotation sensor to fully initialize
+wait(30, MSEC)
+
+
+# Make random actually random
+def initializeRandomSeed():
+    wait(100, MSEC)
+    random = brain.battery.voltage(MV) + brain.battery.current(CurrentUnits.AMP) * 100 + brain.timer.system_high_res()
+    urandom.seed(int(random))
+      
+# Set random seed 
+initializeRandomSeed()
+
+
+def play_vexcode_sound(sound_name):
+    # Helper to make playing sounds from the V5 in VEXcode easier and
+    # keeps the code cleaner by making it clear what is happening.
+    print("VEXPlaySound:" + sound_name)
+    wait(5, MSEC)
+
+# add a small delay to make sure we don't print in the middle of the REPL header
+wait(200, MSEC)
+# clear the console to make sure we don't have the REPL in the console
+print("\033[2J")
+
+
+
+# define variables used for controlling motors based on controller inputs
+drivetrain_l_needs_to_be_stopped_controller_1 = False
+drivetrain_r_needs_to_be_stopped_controller_1 = False
+
+# define a task that will handle monitoring inputs from controller_1
+def rc_auto_loop_function_controller_1():
+    global drivetrain_l_needs_to_be_stopped_controller_1, drivetrain_r_needs_to_be_stopped_controller_1, remote_control_code_enabled
+    # process the controller input every 20 milliseconds
+    # update the motors based on the input values
+    while True:
+        if remote_control_code_enabled:
+            
+            # calculate the drivetrain motor velocities from the controller joystick axies
+            # left = axis3
+            # right = axis2
+            drivetrain_left_side_speed = controller_1.axis3.position()
+            drivetrain_right_side_speed = controller_1.axis2.position()
+            
+            # check if the value is inside of the deadband range
+            if drivetrain_left_side_speed < 5 and drivetrain_left_side_speed > -5:
+                # check if the left motor has already been stopped
+                if drivetrain_l_needs_to_be_stopped_controller_1:
+                    # stop the left drive motor
+                    left_drive_smart.stop()
+                    # tell the code that the left motor has been stopped
+                    drivetrain_l_needs_to_be_stopped_controller_1 = False
+            else:
+                # reset the toggle so that the deadband code knows to stop the left motor next
+                # time the input is in the deadband range
+                drivetrain_l_needs_to_be_stopped_controller_1 = True
+            # check if the value is inside of the deadband range
+            if drivetrain_right_side_speed < 5 and drivetrain_right_side_speed > -5:
+                # check if the right motor has already been stopped
+                if drivetrain_r_needs_to_be_stopped_controller_1:
+                    # stop the right drive motor
+                    right_drive_smart.stop()
+                    # tell the code that the right motor has been stopped
+                    drivetrain_r_needs_to_be_stopped_controller_1 = False
+            else:
+                # reset the toggle so that the deadband code knows to stop the right motor next
+                # time the input is in the deadband range
+                drivetrain_r_needs_to_be_stopped_controller_1 = True
+            
+            # only tell the left drive motor to spin if the values are not in the deadband range
+            if drivetrain_l_needs_to_be_stopped_controller_1:
+                left_drive_smart.set_velocity(drivetrain_left_side_speed, PERCENT)
+                left_drive_smart.spin(FORWARD)
+            # only tell the right drive motor to spin if the values are not in the deadband range
+            if drivetrain_r_needs_to_be_stopped_controller_1:
+                right_drive_smart.set_velocity(drivetrain_right_side_speed, PERCENT)
+                right_drive_smart.spin(FORWARD)
+        # wait before repeating the process
+        wait(20, MSEC)
+
+# define variable for remote controller enable/disable
+remote_control_code_enabled = True
+
+rc_auto_loop_thread_controller_1 = Thread(rc_auto_loop_function_controller_1)
+
+#endregion VEXcode Generated Robot Configuration
+
+remote_control_code_enabled = True
+myVariable = 0
+disableTimer = 0
+
+def when_started1():
+    global myVariable, disableTimer, remote_control_code_enabled
+    disableTimer = 3
+    remote_control_code_enabled = True
+    drivetrain.set_drive_velocity(100, PERCENT)
+    drivetrain.set_turn_velocity(100, PERCENT)
+    brain.screen.set_pen_color(Color.BLUE)
+    brain.screen.set_fill_color(Color.BLUE)
+    brain.screen.draw_rectangle(0, 0, 479, 239)
+
+def onevent_bumper_a_pressed_0():
+    global myVariable, disableTimer, remote_control_code_enabled
+    remote_control_code_enabled = False
+    drivetrain.stop()
+    brain.screen.set_fill_color(Color.RED)
+    brain.screen.draw_rectangle(0, 0, 479, 239)
+    wait(disableTimer, SECONDS)
+    disableTimer = disableTimer * 2
+    brain.screen.set_fill_color(Color.BLUE)
+    brain.screen.draw_rectangle(0, 0, 479, 239)
+    remote_control_code_enabled = True
+
+def onevent_bumper_b_pressed_0():
+    global myVariable, disableTimer, remote_control_code_enabled
+    remote_control_code_enabled = False
+    drivetrain.stop()
+    brain.screen.set_fill_color(Color.RED)
+    brain.screen.draw_rectangle(0, 0, 479, 239)
+    wait(disableTimer, SECONDS)
+    disableTimer = disableTimer * 2
+    brain.screen.set_fill_color(Color.BLUE)
+    brain.screen.draw_rectangle(0, 0, 479, 239)
+    remote_control_code_enabled = True
+
+# system event handlers
+bumper_a.pressed(onevent_bumper_a_pressed_0)
+bumper_b.pressed(onevent_bumper_b_pressed_0)
+# add 15ms delay to make sure events are registered correctly.
+wait(15, MSEC)
+
+when_started1()
